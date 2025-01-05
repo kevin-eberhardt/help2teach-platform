9f946b6b985febe6993074e62e9ac1f2cf3538b7
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2025-01-05 15:25:26 +0100

    rebase for i18n

--- app/(application)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <header>Header</header>
-      <main>{children}</main>
-      <footer>Footer</footer>
-    </div>
-  );
-}
--- app/(public)/register/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function RegistrationPage() {
-  return <h1 className="text-9xl">Registration page</h1>;
-}
