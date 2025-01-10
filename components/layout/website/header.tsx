a3410b4c8b55d07ed6c8b136f2f7b31f209c358d
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2025-01-10 17:10:31 +0100

    changed header layout

--- components/layout/website/header.tsx
+++ components/layout/website/header.tsx
@@ -5,13 +5,12 @@ import UserAvatar from "../../user-avatar";
 
 export default function Header() {
   return (
-    <header className="flex gap-4 p-4 items-center shadow-md">
+    <header className="flex gap-4 p-4 items-center justify-between shadow-md">
       <Link className="flex gap-2 items-center" href="/">
         <Image src="/logo.svg" alt="Logo" width={40} height={40} />
         <span className="font-bold text-primary">Help2Teach</span>
       </Link>
-      <div className="flex-grow">Header</div>
-      <div className="grid grid-cols-2 gap-2 items-center">
+      <div className="flex gap-2 items-center">
         <LocaleSwitcher />
         <UserAvatar />
       </div>
